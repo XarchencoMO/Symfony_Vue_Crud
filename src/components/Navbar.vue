2782c9813dc59242a96c316cf6fc8908f5243163
XarchencoMO <xarchencomo@gmail.com>
2022-07-22 02:04:57 +0700

    added dark mode and responsive navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,34 +1,59 @@
 <template>
-    <v-container fluid>
-      <v-app-bar
-          color="deep-purple accent-4"
+  <v-container fluid>
+    <v-app-bar
+        dense
+    >
+      <div class="hidden-xs-only">
+          <v-btn
+              color="primary"
+              class="ml-3"
+              v-for="item in menu"
+              :to="item.link"
+          >
+            {{ item.text }}
+          </v-btn>
+      </div>
+      <v-spacer/>
+      <v-switch
+          label="Dark mode"
+          hide-details
+          v-model="$vuetify.theme.dark"
+      ></v-switch>
+      <v-app-bar-nav-icon  @click.stop="drawer = !drawer" class="hidden-sm-and-up"></v-app-bar-nav-icon>
+    </v-app-bar>
+    <v-navigation-drawer
+        @click.stop="drawer = !drawer"
+        v-model="drawer"
+        absolute
+        temporary
+    >
+      <v-list
+          nav
           dense
       >
-          <v-btn
-              outlined
-              class="ml-3"
-            >
-            <router-link to="/">Main</router-link>
-          </v-btn>
-          <v-btn outlined class="ml-3">
-            <router-link to="/products">Products table</router-link>
-          </v-btn>
-          <v-btn outlined class="ml-3">
-            <router-link to="/categories">Categories table</router-link>
-          </v-btn>
-        <v-spacer/>
-        <v-app-bar-nav-icon @click="drawer = true"
-                            class="d-flex d-sm-none"
-        ></v-app-bar-nav-icon>
-      </v-app-bar>
-    </v-container>
+        <v-list-item-group
+        >
+          <v-list-item v-for="item in menu" :to="item.link">
+            <v-list-item-title>{{ item.text }}</v-list-item-title>
+          </v-list-item>
+
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+
+  </v-container>
 </template>
 
 <script>
 export default {
-  data: () => {
-    items: ['Main','Products table', 'Categories table']
-  }
+  data: () => ({
+    menu: [
+      {link: '/', text: 'Main'},
+      {link: '/products', text: 'Products table'},
+      {link: '/categories', text: 'Categories table'},
+    ],
+    drawer: null
+  }),
 }
 </script>
 
